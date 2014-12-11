1fe5858e2aac0487c8971cfeee6c8faae15820ea
kay-apao25 <kayapao@yahoo.com>
2014-12-11 09:01:50 +0800

    added archive file for new setup in paver

--- pavement.py
+++ pavement.py
@@ -17,7 +17,35 @@ def run_pylint():
     except BuildFailure:
         pass
 
-@needs('unit_tests', 'lettuce_tests', 'run_pylint')
+@needs('unit_tests', 'lettuce_tests', 'run_pylint', 'sdist')
 @task
 def default():
     pass
+
+from paver.setuputils import setup, find_package_data
+
+package_data = find_package_data()
+entry_points = {
+    'console_scripts': [
+    'run_server = bank.bank_app:main',
+    ]
+}
+
+setup(name='bank_app',
+    version='0.0.1',
+    author='Kay Apao',
+    maintainer='Kay Apao',
+    description='Example application to demonstrate \
+    Python testing techniques.',
+    license='License :: Public Domain',
+    include_package_data=True,
+    packages=['bank'],
+    package_data=package_data,
+    entry_points=entry_points)
+
+@task
+@needs('paver.misctasks.generate_setup',
+    'distutils.command.sdist')
+def sdist():
+    """Generates the setup file and packages up the
+commercial_inventory application."""
